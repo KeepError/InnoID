3aee563c1ef0d486ec41a840c9765c06c5da9bb4
Egor Kuziakov <egor.kuziakov@gmail.com>
2023-09-22 12:15:01 +0300

    (Telegram bot) change api logic

--- TelegramBot/innoid_telegram_bot/api.py
+++ TelegramBot/innoid_telegram_bot/api.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import aiohttp
 
 from settings import settings
@@ -36,7 +38,13 @@ async def get_app_by_id(app_id: str) -> dict:
             return data
 
 
-async def get_user_id_by_id_code(id_code: int) -> str:
+@dataclass
+class IdCodeResponse:
+    user_id: str
+    context: dict
+
+
+async def get_user_id_by_id_code(id_code: int) -> IdCodeResponse:
     async with aiohttp.ClientSession() as session:
         async with session.post(
                 url=f"{BASE_URL}/service/user_id/code",
@@ -49,7 +57,8 @@ async def get_user_id_by_id_code(id_code: int) -> str:
                 raise APIException()
             data = await response.json()
             user_id = data.get("user_id", None)
-            return user_id
+            context = data.get("context", None)
+            return IdCodeResponse(user_id=user_id, context=context)
 
 
 async def create_connection(user_id: str, telegram_id: str) -> None:
--- TelegramBot/innoid_telegram_bot/routers/base.py
+++ TelegramBot/innoid_telegram_bot/routers/base.py
@@ -21,15 +21,19 @@ async def cmd_start(message: Message, command: CommandObject):
         await message.answer("Failed to decode data.")
         return
     id_code = data.get("idCode", None)
-    app_id = data.get("appId", None)
-    redirect_to = data.get("redirect", None)
+    # app_id = data.get("appId", None)
+    # redirect_to = data.get("redirect", None)
 
     if not id_code:
         await message.answer("Not enough data to create connection.")
         return
 
     try:
-        user_id = await api.get_user_id_by_id_code(id_code)
+        id_code_response = await api.get_user_id_by_id_code(id_code)
+        user_id = id_code_response.user_id
+        context = id_code_response.context
+        app_id = context.get("appId", None)
+        redirect_to = context.get("redirect", None)
     except api.APIException:
         await message.answer("Failed to identify InnoID account.")
         return
@@ -45,22 +49,24 @@ async def cmd_start(message: Message, command: CommandObject):
     except api.APIException:
         user = None
 
-    try:
-        app = await api.get_app_by_id(app_id)
-    except api.APIException:
-        app = None
+    app_name = None
+    if app_id:
+        try:
+            app = await api.get_app_by_id(app_id)
+            app_name = app.get("name", None)
+        except api.APIException:
+            pass
 
-    reply_app_name = app["name"] if app else "an app"
     reply_markup = None
     if redirect_to:
         reply_markup = InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(text=f"Return to {reply_app_name}", url=redirect_to)]
+            [InlineKeyboardButton(text=f"Return to {app_name}" if app_name else "Return", url=redirect_to)]
         ])
 
     user_info = f" ({user['email']})" if user else ""
     reply_text = f"Your InnoID account{user_info} is now connected to Telegram!"
 
     if not redirect_to:
-        reply_text += f"\nYou can now return to {reply_app_name}."
+        reply_text += f"\nYou can now return to {app_name}." if app_name else "\nYou can now return."
 
     await message.answer(reply_text, reply_markup=reply_markup)
