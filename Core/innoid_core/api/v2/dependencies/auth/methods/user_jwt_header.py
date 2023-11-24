a9a67d812d7ce53e40f3c8f77366e0270216bb8f
Egor Kuziakov <egor.kuziakov@gmail.com>
2023-11-24 10:05:23 +0300

    fix bearer authentication

--- Core/innoid_core/api/v2/dependencies/auth/methods/user_jwt_header.py
+++ Core/innoid_core/api/v2/dependencies/auth/methods/user_jwt_header.py
@@ -1,20 +1,21 @@
 from typing import Annotated
 
 from fastapi import Depends
-from fastapi.security import HTTPBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from domain.errors import DomainError
 from domain.modules.auth.jwt.usecases import AuthJWTUseCase
 from use_cases import get_auth_jwt_use_case
 from ..common import AuthMethodResult
 
-bearer_scheme = HTTPBearer()
+bearer_scheme = HTTPBearer(auto_error=False)
 
 
 def get_user_jwt_header_auth_result(
         auth_jwt_use_case: Annotated[AuthJWTUseCase, Depends(get_auth_jwt_use_case)],
-        access_token: Annotated[str | None, Depends(bearer_scheme)]
+        access_token: Annotated[HTTPAuthorizationCredentials | None, Depends(bearer_scheme)]
 ) -> AuthMethodResult:
+    access_token = access_token.credentials if access_token else None
     user_id = None
     if access_token:
         try:
